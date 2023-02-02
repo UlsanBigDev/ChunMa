d73a7ddcd17697f507c1047b0b55393ff8b9241f
CaKlan <lhs74187@gmail.com>
2023-02-02 23:57:59 +0900

    todo input text, line modify, checkbox in line add

--- src/page/Modal.tsx
+++ src/page/Modal.tsx
@@ -1,35 +1,45 @@
 import React, {useState} from 'react';
 import '../App.css';
 
-function createLine(lines : Array<JSX.Element>, setLines : React.Dispatch<React.SetStateAction<JSX.Element[]>>){
-    let line : JSX.Element = <li>asdqwedfgert</li>;
+function createLine(lines : Array<JSX.Element>, setLines : React.Dispatch<React.SetStateAction<JSX.Element[]>>, txt : string){
+    if(txt != ""){
+        let line : JSX.Element = (
+            <div className="todo-line">
+                <input type="checkbox"/>
+                <p>{txt}</p>
+            </div>
+            );
+        
+        let temp : Array<JSX.Element> = [];
+        temp = Array.from(lines);
+        temp.push(line);
     
-    let temp : Array<JSX.Element> = [];
-    temp = Array.from(lines);
-    temp.push(line);
-
+        setLines(temp);
+    }
     
-    setLines(temp);
 }
 
 
 function DayInfo(props : IDayInfo){
 
     let [lines, setLines] = useState<Array<JSX.Element>>([]);
+    let [text, setText] = useState<string>("");
 
+    
     return (
         <div className="day-info">
             <div className="today">2023.01.{props.day}</div>
             <br/>
             <div className="day-list">
+                <div style={{display:"flex"}}>
+                    <input type="text" value={text} onChange={(e)=>{setText(e.currentTarget.value)}}/>
+                    <div className="createLine" onClick={() => {createLine(lines as Array<JSX.Element>, setLines, text);}}>추가</div>
+                </div>
                 
-                <div className="createLine" onClick={() => {createLine(lines as Array<JSX.Element>, setLines);}}></div>
                 <br/>
                 <div style={{"overflowY":"scroll","height":"250px"}}>
                 <ul>
-                    {lines ? lines?.map((v, index)=>{
-                        return <div className="todo-line" key={index}>asd</div>
-                    }) : <p>할 일을 추가해보세요</p>}
+                    {lines ? lines : <p>할 일을 추가해보세요</p>}
                 </ul>    
                 </div>
                             
