db447b41f0b2bb6571f87d9a63479a418eedc551
Daram0o0 <c30728535@gmail.com>
2023-02-03 00:27:43 +0900

    Calendar day % 7

--- src/page/Calendar.tsx
+++ src/page/Calendar.tsx
@@ -3,18 +3,55 @@ import logo from './logo.svg';
 import '../App.css';
 
 function Calendar() {
-
+  const monthTemp = ["January","Feburary","March","April","June","July","Agust","September","October","November","December"];
+  const dayTemp = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+    
     // 이전 달의 마지막 날 날짜와 요일 구하기
     let startDay = new Date();
-    let prevDate = startDay.getDate();
-    let prevDay = startDay.getDay();
-    console.log(prevDate, prevDay);
+    let prevYear = startDay.getFullYear(); //연도
+    let prevMonth = startDay.getMonth(); //달 [0,1,2,3,4,5,6,7,8,9,10,11]
+    let prevDate = startDay.getDate(); //날짜 [1,2 ... ?]
+    let prevDay = startDay.getDay(); //요일 [0,1,2,3,4,5,6]
+    let firstDay = new Date(prevYear,prevMonth,1).getDay();
+    let lastDate = new Date(prevYear,prevMonth+1,0).getDate();
+
+    console.log(prevYear, monthTemp[prevMonth], prevDate, dayTemp[prevDay]);
+
+    let weekNum = Math.ceil((prevDay + lastDate) / 7);
+    
+    const arr=[];
+    for (let i = 0; i<firstDay; i++){
+      arr.push(-1);
+    }
+    for (let i = 1; i<=lastDate; i++){
+      arr.push(i);
+    }
+    for (arr.length; arr.length < weekNum * 7; ){
+      arr.push(0);
+    }
 
     return (
       <div>
-        <h1> 달력 만들기 </h1>
+        <center>
+          <h1>{monthTemp[prevMonth]} {prevYear}</h1>
+          {dayTemp.map((day)=>{
+            return(
+              <b><span>{day}{" "}</span></b>
+            )
+          })}
+          <br/>
+          {arr.map((date,index)=>{
+            if ((index+1) % 7 == 0){
+              return <div>{date}<hr/></div>
+            }
+            else {
+              return <div>{date}</div>
+            }
+          })}
+        </center>
       </div>
     );
   }
 
+  
 export default Calendar;
