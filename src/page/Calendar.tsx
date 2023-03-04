e2231da8854cc5b5145811d9833aff54da05b043
whisper74541 <zaq4869@naver.com>
2023-03-04 22:53:26 +0900

    진짜 진짜 최종

--- src/page/Calendar.tsx
+++ src/page/Calendar.tsx
@@ -175,9 +175,9 @@ function Calendar(props : {setModal:Function}) {
           <p>{monthTemp[viewMonth]} {viewYear}</p>
       </div>
       <div className='MonthMove'>
-          <button onClick={() => {beformonth()}}> &lt; </button>
-          <button onClick={() => {gotoday()}}> Today </button>
-          <button onClick={() => {frontmonth()}}> &gt; </button>
+          <button className ='btn' onClick={() => {beformonth()}}> &lt; </button>
+          <button className ='btn-today' onClick={() => {gotoday()}}> Today </button>
+          <button className ='btn' onClick={() => {frontmonth()}}> &gt; </button>
       </div>
       <div className='week'>
         {weekArr.map((arr, index)=>{
@@ -371,7 +371,7 @@ function Calendar(props : {setModal:Function}) {
             return <div className='calendar-week'> {value} </div>
           })
         } */}
-      </div>        
+      </div>
       </div>
     );
   }
