259a6ebd9ed02501aeea260638967a97c4faa7f6
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-03-08 00:23:27 +0900

    day info renderer modify

--- src/component/DayInfo.tsx
+++ src/component/DayInfo.tsx
@@ -10,8 +10,6 @@ interface ToDoList {
     uid : string;
     List : string;
 }
-
-
 function ListItem(props : IListItem){
 
     let [isChecked,setIsChecked] = useState<boolean>();
@@ -29,8 +27,6 @@ export default function DayInfo(props : IDayInfo){
     let [list, setList] = useState<ToDoList[] | null>();
     let [lines, setLines] = useState<Array<JSX.Element>>([]);
 
-    let [datastate, setDatastate] = useState<string>("loding....");
-
     useEffect(() => {
         fetch(`https://project-calendar-701d3-default-rtdb.firebaseio.com/ToDoList/${props.year}/${props.month}/${props.day}.json`, {
             method : 'GET'
@@ -38,28 +34,23 @@ export default function DayInfo(props : IDayInfo){
         .then(res => {
             return res.json();
         })
-        .then(data => {
-            // 아래에서 출력하지 않고 'GET'할 때 조건을 줘서 출력하기
-            // 데이터가 있을 때는 datastate를 loding... => Yes Data로 변경해주기
-            // 데이터가 없을 때는 datastate를 loding... => No Data로 변경해주기
-            if (data != undefined && data != null) {
-                setTimeout(()=>{
-                    setDatastate("Yes Data");
-                    const arr = Object.entries<ToDoList>(data);
-                    const temp = list == null ? [] : [...list];
-                    arr.forEach((value) => {
-                        value[1] = {
-                            ...value[1],
-                            uid : value[0]
-                        }
-                        temp.push(value[1]);
-                    });
-                    setList(temp);
-                },1000);
-            } else {
-                setDatastate("No Data");
+        .then(data => {            
+            if(data == null) {
+                setList([]);
+                return;
             }
-        });
+            const arr = Object.entries<ToDoList>(data);
+            const temp = list == null ? [] : [...list];
+            arr.forEach((value) => {
+                value[1] = {
+                    ...value[1],
+                    uid : value[0]
+                }
+                temp.push(value[1]);
+            });
+            setList(temp);
+        });        
+            
     },[]);
 
     
@@ -72,15 +63,8 @@ export default function DayInfo(props : IDayInfo){
         if(text != ""){
             let line : JSX.Element = (
                 <ListItem testtext={text}/>
-            );
+            );            
             
-            // 아련한 복사와 같다
-            // let temp : Array<JSX.Element> = [];
-            // temp = Array.from(lines);
-            // console.log(line);
-            // temp.push(line);
-            
-            // console.log(temp);
             setLines([...lines, line]);
             const ToDoList = {
                 List : text
@@ -103,8 +87,6 @@ export default function DayInfo(props : IDayInfo){
 
     let [is2Checked, setIs2Checked] = useState<boolean>();
 
-    console.log(list)
-
     return (        
         <div className="day-info"> {/* 모달 창 */}
             <div className="today">{props.year}.{props.month}.{props.day}</div>
@@ -118,30 +100,22 @@ export default function DayInfo(props : IDayInfo){
                     <div style={{"overflowY":"scroll","height":"250px"}}>
                         <ul>
                             {
-                                // datastate가 loding.... 라면
-                                datastate == "loding...." ?
-                                // datastate (loding....)를 출력
-                                <li>
-                                    {datastate}
-                                </li> :
-                                // datastate가 loding....가 아니라면
-                                // datastate가 No Data라면
-                                datastate == "No Data" ?
-                                // datastate (No Data)를 출력
-                                <li>
-                                    {datastate}
-                                </li> :
-                                // datastate가 No Data가 아니라면
-                                // list.map를 출력
-                                list?.map((value : ToDoList , index) => {
-                                    return (
-                                        <li className="todo-line">
-                                            <input type="checkbox" onChange={(e)=>{setIs2Checked(e.target.checked);}}/>
-                                            <p style={is2Checked ? {textDecoration: "line-through"} : {textDecoration:"none"}} >{value.List} </p>
-                                            <div className = "createLine" onClick = {() => {deleteList(value)}}> 삭제 </div>
-                                        </li>
+                                list == null 
+                                ? <li>로딩중</li> 
+                                : list.length <= 0
+                                    ? <li>No Data</li>
+                                    : (
+                                        list.map((value:ToDoList)=>{
+                                            return(
+                                                <li className="todo-line">
+                                                    <input type="checkbox" onChange={(e)=>{setIs2Checked(e.target.checked);}}/>
+                                                    <p style={is2Checked ? {textDecoration: "line-through"} : {textDecoration:"none"}} >{value.List} </p>
+                                                    <div className = "createLine" onClick = {() => {deleteList(value)}}> 삭제 </div>
+                                                </li>
+                                            );
+                                        })                                    
                                     )
-                                })
+
                             }
                         </ul>
                     </div>
@@ -149,3 +123,13 @@ export default function DayInfo(props : IDayInfo){
         </div>
     );
 }
+
+/**
+ * 
+ *  <li className="todo-line">
+        <input type="checkbox" onChange={(e)=>{setIs2Checked(e.target.checked);}}/>
+        <p style={is2Checked ? {textDecoration: "line-through"} : {textDecoration:"none"}} >{value.List} </p>
+        <div className = "createLine" onClick = {() => {deleteList(value)}}> 삭제 </div>
+    </li>
+ * 
+ */
